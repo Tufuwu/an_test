06bcf444bbff408189b6893c3d6abcf889ab9205
wu <wu@.com>
2026-06-09 02:51:26 +0800

    thegrill/grill

--- README.md
+++ README.md
@@ -1,45 +1,25 @@
-[![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.1341346.svg)](https://doi.org/10.5281/zenodo.1341346)
-[![Build Status](https://travis-ci.com/raysect/source.svg?branch=master)](https://travis-ci.com/raysect/source)
+# 👨‍🍳 The Grill
+[![Build Status](https://travis-ci.org/thegrill/grill.svg?branch=main)](https://travis-ci.org/thegrill/grill)
+[![codecov](https://codecov.io/gh/thegrill/grill/branch/main/graph/badge.svg)](https://codecov.io/gh/thegrill/grill)
+[![Documentation Status](https://readthedocs.org/projects/grill/badge/?version=latest)](https://grill.readthedocs.io/en/latest/?badge=latest)
+[![PyPI version](https://badge.fury.io/py/grill.svg)](https://badge.fury.io/py/grill)
+[![PyPI](https://img.shields.io/pypi/pyversions/grill.svg)](https://pypi.python.org/pypi/grill)
 
-<a name="logo"/>
-<a href="https://www.raysect.org/" target="_blank">
-<img src="https://raysect.github.io/documentation/_images/RaysectLogo_small.png" alt="Raysect Logo"></img>
-</a>
+> Cook digital.
 
-Raysect Python Raytracing Package
-=================================
+`The Grill` is a toolset that helps with digital content creation. This is the `grill` namespace, a meta-package with core tools and philosophy guidelines.
 
-A ray-tracing framework for optical/non-optical physics simulations.
+With future users and readers in mind, `The Grill` aims to stick to the following principles:
 
-The aims of the Raysect project are as follows:
+- [DRY (don't repeat yourself)](https://en.wikipedia.org/wiki/Don%27t_repeat_yourself)
+- [SSOT (single source of truth)](https://en.wikipedia.org/wiki/Single_source_of_truth)
+- [Convention over configuration (sensible defaults)](https://en.wikipedia.org/wiki/Convention_over_configuration)
+- [SOLID (understandable, flexible and maintainable)](https://en.wikipedia.org/wiki/SOLID)
+- [KISS (keep it simple)](https://en.wikipedia.org/wiki/KISS_principle)
 
-* develop a ray-tracer that is easy for scientists and engineers to use and extend
+Foundational tools:
+- [Python](https://docs.python.org/3/)
+- [USD](https://graphics.pixar.com/usd/docs/index.html)
+- [EdgeDB](https://edgedb.com)
 
-* the raytracer must be robust and high precision
-
-The general development philosophy is ease of use trumps performance, but performance is not to be ignored.
-
-For more information, see the [documentation pages](http://www.raysect.org/).
-
-
-![Dispersion of light passing through a prism](docs/source/demonstrations/optics/prism_720x405.png)
-
-*Caption: a simulation of Newton's classic experiment, white light being dispersed by a glass prism.*
-
-
-Please note, this code is currently under heavy development and subject to change.
-
-Raysect currently only supports Linux. Windows support will come at a later stage.
-
-Please note, for legal reasons we require the copyright to any contributed code to be passed to the Raysect project. Please see CONTRIBUTING.txt in the repository.
-
-Development Team
-----------------
-
-* Dr Alex Meakins
-* Dr Matthew Carr
-
-Citing the Code
----------------
-
-* Dr Alex Meakins, & Matthew Carr. (2018, August 7). raysect/source: v0.5.2 Release (Version v0.5.2). Zenodo. http://doi.org/10.5281/zenodo.1341376
+Please [visit the docs](https://grill.rtfd.io) for a full walk-through!
--- setup.py
+++ setup.py
@@ -1,112 +1,54 @@
-from setuptools import setup, find_packages, Extension
-import sys
-import numpy
-import os
-import os.path as path
-import multiprocessing
+import itertools
+from pathlib import Path
 
-multiprocessing.set_start_method('fork')
+import setuptools
+from distutils.command.build import build
+from setuptools.command.develop import develop
+from setuptools.command.easy_install import easy_install
+from setuptools.command.install_lib import install_lib
 
-use_cython = True
-force = False
-profile = False
-line_profile = False
-annotate = False
 
-if "--skip-cython" in sys.argv:
-    use_cython = False
-    del sys.argv[sys.argv.index("--skip-cython")]
+# Hopefully all the following will not be needed after PEP 648
+# Reference: https://github.com/pytest-dev/pytest-cov/blob/daf54e79fcb8f549699d28e691302a9251f7e54b/setup.py#L145-L151
+def _copy_pth(obj, install_dir):
+    pth_src = Path(__file__).parent / "grill.pth"
+    pth_tgt = str(Path(install_dir) / pth_src.name)
+    obj.copy_file(str(pth_src), pth_tgt)
+    return [pth_tgt]
 
-if "--force" in sys.argv:
-    force = True
-    del sys.argv[sys.argv.index("--force")]
 
-if "--profile" in sys.argv:
-    profile = True
-    del sys.argv[sys.argv.index("--profile")]
+class BuildPTH(build):
+    def run(self):
+        super().run()
+        _copy_pth(self, self.build_lib)
 
-if "--line-profile" in sys.argv:
-    line_profile = True
-    del sys.argv[sys.argv.index("--line-profile")]
 
-if "--annotate" in sys.argv:
-    annotate = True
-    sys.argv.remove("--annotate")
+class DevelopPTH(develop):
+    def run(self):
+        super().run()
+        _copy_pth(self, self.install_dir)
 
-source_paths = ['raysect', 'demos']
-compilation_includes = [".", numpy.get_include()]
-compilation_args = ['-O3']
-cython_directives = {
-    # 'auto_pickle': True,
-    'language_level': 3
-}
-setup_path = path.dirname(path.abspath(__file__))
 
-if line_profile:
-    compilation_args.append("-DCYTHON_TRACE=1")
-    compilation_args.append("-DCYTHON_TRACE_NOGIL=1")
-    cython_directives["linetrace"] = True
+class EasyInstallPTH(easy_install):
+    def run(self):
+        super().run()
+        _copy_pth(self, self.install_dir)
 
-if use_cython:
 
-    from Cython.Build import cythonize
+class InstallLibPTH(install_lib):
+    def run(self):
+        super().run()
+        self.outputs = _copy_pth(self, self.install_dir)
 
-    # build .pyx extension list
-    extensions = []
-    for package in source_paths:
-        for root, dirs, files in os.walk(path.join(setup_path, package)):
-            for file in files:
-                if path.splitext(file)[1] == ".pyx":
-                    pyx_file = path.relpath(path.join(root, file), setup_path)
-                    module = path.splitext(pyx_file)[0].replace("/", ".")
-                    extensions.append(Extension(module, [pyx_file], include_dirs=compilation_includes, extra_compile_args=compilation_args),)
+    def get_outputs(self):
+        return itertools.chain(super().get_outputs(), self.outputs)
 
-    if profile:
-        cython_directives["profile"] = True
 
-    # generate .c files from .pyx
-    extensions = cythonize(extensions, nthreads=multiprocessing.cpu_count(), force=force, compiler_directives=cython_directives, annotate=annotate)
-
-else:
-
-    # build .c extension list
-    extensions = []
-    for package in source_paths:
-        for root, dirs, files in os.walk(path.join(setup_path, package)):
-            for file in files:
-                if path.splitext(file)[1] == ".c":
-                    c_file = path.relpath(path.join(root, file), setup_path)
-                    module = path.splitext(c_file)[0].replace("/", ".")
-                    extensions.append(Extension(module, [c_file], include_dirs=compilation_includes, extra_compile_args=compilation_args),)
-
-# parse the package version number
-with open(path.join(path.dirname(__file__), 'raysect/VERSION')) as version_file:
-    version = version_file.read().strip()
-
-setup(
-    name="raysect",
-    version=version,
-    url="http://www.raysect.org",
-    author="Dr Alex Meakins et al.",
-    author_email="developers@raysect.org",
-    description='A Ray-tracing Framework for Science and Engineering',
-    license="BSD",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Science/Research",
-        "Intended Audience :: Education",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Natural Language :: English",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Cython",
-        "Programming Language :: Python :: 3",
-        "Topic :: Multimedia :: Graphics :: 3D Rendering",
-        "Topic :: Scientific/Engineering :: Physics"
-    ],
-    install_requires=['numpy', 'matplotlib'],
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    ext_modules=extensions
+setuptools.setup(
+    cmdclass={
+        "build": BuildPTH,
+        "develop": DevelopPTH,
+        "easy_insall": EasyInstallPTH,
+        "install_lib": InstallLibPTH,
+    },
 )
